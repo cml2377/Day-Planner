081a74cc085980593ad883c76af3f621588b740b
Crystal Ly <crystal.m.ly@gmail.com>
2019-12-18 20:48:15 -0600

    coded the script to check EVERY SECOND whether the hour has changed, and to change the class of the div accordingly.

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,30 +7,37 @@ $(document).ready(function () {
 
     var update = function () {
         currentDayEl.text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+
+        var now = moment().hour(); //returns 0 through 23, aka military time 
+
+        var inputBlockEl = $(".inputBlock");
+        //if block is < #; then change class to past            
+        //if block == #; then change class to present
+        //if block > #, then change class to future
+
+        //Then, we calculate the time of day with moment, and then change the classes of the rows accordingly.
+
+        inputBlockEl.each(function (i, element) {
+            element = $(element);
+            if (now > element.attr("data-hour")) {
+                element.addClass("past").removeClass("future present");
+            } else if (now == element.attr("data-hour")) {
+                element.addClass("present").removeClass("future past");
+            }
+        });
     }
+    // we're just gonna check every SECOND.
     setInterval(update, 1000);
 
-    //Then, we calculate the time of day with moment, and then change the classes of the rows accordingly.
-    var nineAM = $("#9Row");
-    var tenAM = $("#10Row");
-    var elevenAM = $("#11Row");
-    var noon = $("#12Row");
-    var onePM = $("#13Row");
-    var twoPM = $("#14Row");
-    var threePM = $("#15Row");
-    var fourPM = $("#16Row");
-    var fivePM = $("#17Row");
-
-    //if time is #AM-- 
-    moment()
 
 
-    //if block is < #; then change class to past            
-    //if block == #; then change class to present
-    //if block > #, then change class to future
 
-    $(".future").addClass(".present").removeClass(".future");
-    $(".present").addClass(".past").removeClass(".present");
+
+
+
+
+
+
 
     //When user clicks on an empty input box, it should allow user to populate it with a string. 
 
