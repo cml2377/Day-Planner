ff7d7f5b01166c87d588bfe5f84a4e723b5ffe02
Crystal Ly <crystal.m.ly@gmail.com>
2019-12-19 18:58:14 -0600

    Stored each individual description into each textArea. Fixed the clear function to clear when you click on button

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,10 +11,10 @@ $(document).ready(function () {
     var update = function () {
         currentDayEl.text(moment().format('MMMM Do YYYY, h:mm:ss a'));
 
-        var now = moment().hour(); //returns 0 through 23, aka military time 
-        //if inputblockEl is < now; then change class to past            
-        //if inputblockEl == now; then change class to present
-        //if inputblockEl > now, then change class to future
+        var now = moment().hour(); //returns 0 through 23, aka military time.
+        //if inputblockEl is < now; then change class to past.            
+        //if inputblockEl == now; then change class to present.
+        //if inputblockEl > now, then change class to future !!! This is NOT needed because no past will become future for the day. Also, all blocks are set as future as default.
 
         //Then, we calculate the time of day with moment, and then change the classes of the rows accordingly.
 
@@ -37,32 +37,34 @@ $(document).ready(function () {
         var button = $(this);
 
         var btnData = $(this).attr("data-time");
-
+        //console log click on THIS specific button in this class
         console.log(btnData);
-        //textArea locates the specific description element that user filled in text with.
-
+        //descriptionTarget locates the specific description element that user filled in text with.
         var descriptionTarget = $("#description" + btnData);
-
         console.log(descriptionTarget);
-
+        //we log to ensure we're grabbing the user input.
         console.log(descriptionTarget.val());
 
-
-
-        var textArea = $(".description" + button.attr("data-time"));
-        console.log(textArea);
-        //userText is the actual text being saved
-        var userText = textArea.val();
-        //this is supposed to set the text area text to that specific textArea and store it in an arry of objects called data.
-        localStorage.setItem(data[textArea.attr("id", userText)]);
+        //this is supposed to set the text area text to that specific textArea and store it.
+        //We pick the "id" attribute from the element, descriptionTarget. This id is the key, and the key value is descriptionTarget.val().
+        localStorage.setItem(descriptionTarget.attr("id"), descriptionTarget.val());
     });
 
     //site should load any saved data from localStorage.
-    textArea = JSON.parse(localStorage.userText);
+    $("#description9").text(localStorage.getItem("description9"));
+    $("#description10").text(localStorage.getItem("description10"));
+    $("#description11").text(localStorage.getItem("description11"));
+    $("#description12").text(localStorage.getItem("description12"));
+    $("#description13").text(localStorage.getItem("description13"));
+    $("#description14").text(localStorage.getItem("description14"));
+    $("#description15").text(localStorage.getItem("description15"));
+    $("#description16").text(localStorage.getItem("description16"));
+    $("#description17").text(localStorage.getItem("description17"));
 
     //clear button clears local storage.
-    $("#clear").on("click", function () {
+    $("#clearBtn").on("click", function () {
         localStorage.clear();
+        $(".description").text("");
     });
 
 });
